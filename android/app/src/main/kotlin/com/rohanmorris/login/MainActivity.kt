e7bea663a17f348e98ac1a20ccbfaf775929d39a
Khadeil Wallace <tech57@vmbs.com>
2020-03-04 16:08:58 -0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/rohanmorris/login/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.rohanmorris.login
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
